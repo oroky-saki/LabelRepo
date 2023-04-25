898b07054815bcaf98be2a5b68a6edde29b9a574
oroky-saki <misha-vav17@mail.ru>
2023-04-25 14:16:47 +0500

    set up the maptract

--- src/main/java/com/label/mylabel/mapper/TrackMapper.java
+++ src/main/java/com/label/mylabel/mapper/TrackMapper.java
@@ -3,12 +3,12 @@ package com.label.mylabel.mapper;
 import com.label.mylabel.entity.TrackEntity;
 import com.label.mylabel.model.Track;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Mapper(componentModel="spring")
-@Component
+@Mapper(componentModel="spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface TrackMapper {
 
     Track toDto(TrackEntity trackEntity);
